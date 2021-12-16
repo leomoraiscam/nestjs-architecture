9f856879e067afce88edc2ad032787e59e844d09
lmorais <leozonnn@gmail.com>
2021-12-16 16:32:26 -0300

    feat: implementation entity file and create domain layer

---
+++ src/modules/song/domain/entity/Song.entity.ts
@@ -0,0 +1,13 @@
+export class Song {
+  title: string;
+
+  artist: string;
+
+  lyric: string;
+
+  constructor(title: string, artist: string, lyric: string) {
+    this.title = title;
+    this.artist = artist;
+    this.lyric = lyric;
+  }
+}
