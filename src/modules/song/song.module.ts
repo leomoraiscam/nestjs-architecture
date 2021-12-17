0ea50807849be01ea0805e4707751a220232674b
lmorais <leozonnn@gmail.com>
2021-12-17 14:22:05 -0300

    chore: add presentation in exports and provider module

--- src/modules/song/song.module.ts
+++ src/modules/song/song.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common'
 import { SongController } from './http/controller/song.controller'
 import { SongService } from './application/song.service'
 import { SongRepository } from './infrasctructure/song.repository'
+import { SongPresentation } from './presentation/song.presentation'
 
 @Module({
   controllers: [SongController],
-  providers: [SongService, SongRepository]
+  providers: [SongPresentation, SongService, SongRepository],
+  exports: [SongPresentation]
 })
 
 export class SongModule { }
